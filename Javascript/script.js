0f853b87c7104cba498b35e1445996a80531f3b6
Elena Guzman <elena.guzman@snhu.edu>
2025-10-21 22:32:23 -0400

    Added enhanced click function to accept user input

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,6 @@
 /* CS465 Lab2 Javascript file */
 
-
+var list = [];
 var map = L.map('map').setView([43.0382166,-71.4519303], 14); //Set map starting view point and zoom level
 
 
@@ -17,5 +17,9 @@ function onMapClick(e){
         .setLatLng(e.latlng)
         .setContent("The coordinates you clicked are " + e.latlng.toString())
         .openOn(map);
+    var description = prompt("Enter Description:");
+    var marker = L.marker(e.latlng, {title:description}).addTo(map);
+    list.push(marker);
+    console.log (list);
 }
 map.on('click', onMapClick);
