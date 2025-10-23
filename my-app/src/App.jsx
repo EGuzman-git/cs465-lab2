e64c7791100bdf47825cd947b09a17f6e2f14837
Elena Guzman <elena.guzman@snhu.edu>
2025-10-22 23:25:53 -0400

    Added Done button and Reset

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,24 +1,42 @@
     /* CS465 Lab2 Javascript React file */
-import { useEffect, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import { jsx } from "react/jsx-runtime";
 
 export function App() {
-  const [poi,setpoi] = useState([]);
-  const [prompt,setprompt] = useState("Click on the map to add a point of interest");
+  const [poiList,setpoi] = useState([]);
+  const [showList,setShowList] = useState(true);
+  const [resetMap,setResetMap] = useState(0);
   function addMarker(newMarker) {
     setpoi(newMarker)
   }
+  function doneAdding() {
+    setShowList(false);
+  }
+  function resetMapButton() {
+    setpoi([]);
+    setShowList(true);
+  }
 
-  return (<div>
-  <ul><h1>{prompt}</h1>{poi}</ul>
-      <Map addMarkerFromMap={addMarker} />
-      </div>
-
-  );
+  return (
+    <div>
+      {showList ? 
+        <ul>
+          <h1>Click on the map to add a point of interest</h1>
+          {poiList}
+          <button onClick={doneAdding}>Done Adding Points</button>
+        </ul>
+      :
+      ""
+      }
+      <Map addMarkerFromMap={addMarker} allowMoreMarkers={showList} resetMap={resetMapButton}/>
+      <button onClick={resetMapButton}>Reset</button>
+    </div>
+  )
 }
 
 
-export function Map({addMarkerFromMap}) {
+
+export function Map({addMarkerFromMap,allowMoreMarkers,resetMap}) {
 
   useEffect(() => {
 
@@ -34,26 +52,34 @@ export function Map({addMarkerFromMap}) {
 
     //Map click response functionality
     function onMapClick(e){
-        popup
-            .setLatLng(e.latlng)
-            .setContent("The coordinates you clicked are " + e.latlng.toString())
-            .openOn(map);
-        var description = prompt("Enter Description:");
-        var marker = L.marker(e.latlng, {title:description}).addTo(map);
-        markerList.push(marker);
-        var formattedMarkerList = markerList.map(point => {
-          return <li>{point.options.title}</li>
-        });
-        
-        addMarkerFromMap(formattedMarkerList);
+
+      //if allowMoreMarkers is false do not allow any more markers
+      if (!allowMoreMarkers){
+        return;
+      }
+
+      popup
+          .setLatLng(e.latlng)
+          .setContent("The coordinates you clicked are " + e.latlng.toString())
+          .openOn(map);
+      var description = prompt("Enter Description:");
+      var marker = L.marker(e.latlng, {title:description}).addTo(map);
+      markerList.push(marker);
+      var formattedMarkerList = markerList.map((point, index) => {
+        return <li key={index}>"{point.options.title}" @ {marker._latlng.toString()} </li>
+      });
+      
+      addMarkerFromMap(formattedMarkerList);
     }
 
     map.on('click', onMapClick);
 
+    // Function that gets called when component goes away(Part of useEffect())
     return () => {
       map.remove();
+      markerList = [];
   	};
-  }, [App]);
+  }, [App ]);
   
   return <div id="map"></div>
 }
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -7,6 +7,5 @@ import {App, Map} from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  {/* <Map /> */}
   </StrictMode>,
 )
